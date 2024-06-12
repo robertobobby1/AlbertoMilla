4506e3c324f70f766101e1a656dcadb2ad9020a0
Roberto Milla Martinez <roberto@accounton.io>
2024-06-12 15:49:48 +0200

    fix: studio text

--- src/routes/Estudio.js
+++ src/routes/Estudio.js
@@ -13,38 +13,16 @@ function Estudio() {
   return (
     <div>
       <ProjectGallery images={images} />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginBottom: "40px",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            marginRight: "20px",
-            maxWidth: "250px",
-          }}
-        >
+      <div class="flex justify-center mb-10 ml-5 mr-5">
+        <div class="flex flex-col max-w-4xl text-justify">
           Nuestro estudio de arquitectura y urbanismo nace en el año 1981, y
           desde entonces hemos perseguido dotar de un valor excepcional a todos
           nuestros trabajos. La calidad en nuestra especialización ha sido el
           estandarte que ha encabezado siempre a nuestro estudio para acometer
-          cada uno de los proyectos a los que nos hemos enfrentado.
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            maxWidth: "200px",
-          }}
-        >
-          El grupo de profesionales que forman y colaboran con el estudio,
-          siempre ha tenido como principal premisa la búsqueda de las
-          necesidades del cliente para obtener la mayor calidad plástica y econ
-          ómica en el proyecto.
+          cada uno de los proyectos a los que nos hemos enfrentado. El grupo de
+          profesionales que forman y colaboran con el estudio, siempre ha tenido
+          como principal premisa la búsqueda de las necesidades del cliente para
+          obtener la mayor calidad plástica y económica en el proyecto.
           <br />
           <br />
           Alberto Milla
